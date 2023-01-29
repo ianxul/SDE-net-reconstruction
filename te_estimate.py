d3ffa19488d1df34b1e3f4a918bfb963548fabf9
Ian Xul Belaustegui <ianxulb@gmail.com>
2023-01-29 10:36:48 -0600

    Added a script to calculate TE between two nodes using the idtxl package.

--- te_estimate.py
+++ te_estimate.py
@@ -3,21 +3,30 @@ import numpy as np
 from idtxl.estimators_jidt import JidtKraskovTE
 from idtxl.estimators_opencl import OpenCLKraskov
 
-def get_TE_estimator(history_source = 1, history_target = 1, delay = 1, tau_source = 1, tau_target = 1, gpu = False):
+def get_TE_estimator(settings, gpu = False):
     if not gpu:
-        return JidtKraskovTE(history_source, history_target, tau_source, tau_target, delay)
+        return JidtKraskovTE(settings = settings)
 
 ## This function will take data as a numpy array of shape (n,m) where n is the number of nodes and m the number of datapoints for each.
 def generalte_TE_mat(data, history_source = 1, history_target = 1, delay = 1, tau_source = 1, tau_target = 1, gpu = False):
     n, m = data.shape
 
-    TE_mat = np.full((n,n), None)
+    TE_mat = np.full((n,n), 0.0)
 
-    TE_estimator = get_TE_estimator(history_source, history_target, delay, tau_source, tau_target, gpu)
+    settings = dict({
+        'history_source' : history_source,
+        'history_target' : history_target,
+        'tau_source' : tau_source,
+        'tau_target' : tau_target,
+        'source_target_delay' : delay
+    })
+    TE_estimator = get_TE_estimator(settings, gpu)
 
     for i in range(n):
         for j in range(n):
-            if i == j: continue
+            if i == j: 
+                TE_mat[i,i] = 1.0
+                continue
             TE_mat[i,j] = TE_estimator.estimate(data[i,:],data[j,:])
 
     return TE_mat
