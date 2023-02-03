31b9a930d514366c6ad07211fb36b2a3289e4294
Ian Xul Belaustegui <ianxulb@gmail.com>
2023-02-03 11:31:02 -0600

    Added gpu estimator in the te_estimate script. Must check in Tralfamador if all works good.

--- te_estimate.py
+++ te_estimate.py
@@ -5,28 +5,39 @@ from idtxl.estimators_opencl import OpenCLKraskov
 
 def get_TE_estimator(settings, gpu = False):
     if not gpu:
-        return JidtKraskovTE(settings = settings)
+        return 
+    else:
+        return 
 
 ## This function will take data as a numpy array of shape (n,m) where n is the number of nodes and m the number of datapoints for each.
 def generalte_TE_mat(data, history_source = 1, history_target = 1, delay = 1, tau_source = 1, tau_target = 1, gpu = False):
     n, m = data.shape
 
     TE_mat = np.full((n,n), 0.0)
+    if not gpu:
+        settings = dict({
+            'history_source' : history_source,
+            'history_target' : history_target,
+            'tau_source' : tau_source,
+            'tau_target' : tau_target,
+            'source_target_delay' : delay
+        })
 
-    settings = dict({
-        'history_source' : history_source,
-        'history_target' : history_target,
-        'tau_source' : tau_source,
-        'tau_target' : tau_target,
-        'source_target_delay' : delay
-    })
-    TE_estimator = get_TE_estimator(settings, gpu)
+        TE_estimator = JidtKraskovTE(settings = settings)
 
-    for i in range(n):
-        for j in range(n):
-            if i == j: 
-                TE_mat[i,i] = 1.0
-                continue
-            TE_mat[i,j] = TE_estimator.estimate(data[i,:],data[j,:])
+        for i in range(n):
+            for j in range(n):
+                if i == j: 
+                    TE_mat[i,i] = 1.0
+                    continue
+                TE_mat[i,j] = TE_estimator.estimate(data[i,:],data[j,:])
+    else:
+        TE_estimator = OpenCLKraskov()
+        for i in range(n):
+            for j in range(n):
+                if i == j: 
+                    TE_mat[i,i] = 1.0
+                    continue
+                TE_mat[i,j] = TE_estimator.estimate(data[i,1:],data[j,:-1],data[i,:-1])
 
     return TE_mat
